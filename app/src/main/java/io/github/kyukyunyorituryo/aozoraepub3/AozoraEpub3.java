f7ab97d76bc50d6418fcbe0e3b32c35766ff14c5
急急如律令 <kyukyunyorituryo@live.jp>
2026-04-24 01:07:25 +0900

    1.1.1b31Q相当

--- app/src/main/java/io/github/kyukyunyorituryo/aozoraepub3/AozoraEpub3.java
+++ app/src/main/java/io/github/kyukyunyorituryo/aozoraepub3/AozoraEpub3.java
@@ -38,7 +38,7 @@ import com.github.junrar.rarfile.FileHeader;
 /** コマンドライン実行用mainとePub3変換関数 */
 public class AozoraEpub3
 {
-	public static final String VERSION = "1.1.1b30Q";
+	public static final String VERSION = "1.1.1b31Q";
 
 	/** コマンドライン実行用 */
 	public static void mainthread(Context context, File srcFile)  {
